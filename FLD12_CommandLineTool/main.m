798c558496ae547593ffe9f896ddb5972886ebcd
PeterCoolAssHuber <peterhub@peteserver.local>
2017-09-16 15:21:56 -0400

    Initial Commit

---
+++ FLD12_CommandLineTool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FLD12_CommandLineTool
+//
+//  Created by PeterCoolAssHuber on 2017-09-16.
+//  Copyright © 2017 Peter Huber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
